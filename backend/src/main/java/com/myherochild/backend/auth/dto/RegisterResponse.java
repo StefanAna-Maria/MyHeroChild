c284801a8ac2d1bc16c3e2789465c2265c0e2d44
Ana-Maria Stefan <ana.maria.stefan03@gmail.com>
2026-03-05 16:39:50 +0200

    Added RegisterResponse for parentCode generation

---
+++ backend/src/main/java/com/myherochild/backend/auth/dto/RegisterResponse.java
@@ -0,0 +1,16 @@
+package com.myherochild.backend.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterResponse {
+
+    private String parentCode;
+
+}
